39a982611d3ae34ac606625fe585e84b801c1638
iubkud <mbarry@gmail.com>
2016-05-08 13:32:22 -0400

    fixing typo in prod settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'shielded-depths-46469.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smpt_settings = {
+  ActionMailer::Base.smtp_settings = {
     :address        => 'smpt.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
