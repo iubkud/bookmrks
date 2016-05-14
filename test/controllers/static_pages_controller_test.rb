193129bb857265f8f3bb6233771ec520642dfa74
iubkud <mbarry@gmail.com>
2016-05-14 16:42:43 -0400

    shows current user page if going to root and logged in

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
+  def setup
+    @user = users(:matt)
+  end
+
   test "should get home" do
     get :home
     assert_response :success
@@ -16,4 +21,9 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should redirect to user if logged in" do
+    log_in_as(@user)
+    get :home
+    assert_redirected_to @user   
+  end
 end
