df76bcff9ee697883cba090e0281e284fb1c7829
iubkud <mbarry@gmail.com>
2016-06-18 17:25:36 -0400

    account activations json

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,14 +1,15 @@
 class AccountActivationsController < ApplicationController
+  respond_to :json
   def edit
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
-      flash[:success] = "Account activated!"
-      redirect_to user
+      render :json => { :activated => "true",
+                        :message   => "Account activated!" }
     else
-      flash[:danger] = "Invalid activation link"
-      redirect_to root_url
+      render :json => { :activated => "false",
+                        :message   => "Error activating account." }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,4 @@ Rails.application.routes.draw do
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :bookmarks,           only: [:create, :destroy]
   resources :tags,                only: [:show]
-  # resources :folders,             only: [:show, :create, :destroy]
 end
